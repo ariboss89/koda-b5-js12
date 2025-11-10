40203ea03877bc37a3bc53f0efe6cd921f526486
ariramadhan <ariboss89@gmail.com>
2025-11-10 11:31:33 +0700

    fetch-email-city-name-using-method-toLowerCase-and-without-toLowerCase

--- minitask12.js
+++ minitask12.js
@@ -1,10 +1,13 @@
 const url = "https://jsonplaceholder.typicode.com/users";
 
-const arrEmail = [];
-const arrEmail2 = [];
+const arrUser = [];
+const arrUser2 = [];
 
 fetch(url)
   .then(function (result) {
+    if(!result.ok){
+      throw new Error(`${result.status} ${result.statusText}`);
+    }
     return result.json();
   })
   .then(function (body) {
@@ -28,26 +31,33 @@ async function requestDataAsync() {
   }
 }
 
+//Using built in method
 async function processUserData(data) {
   for (let a = 0; a < data.length; a++) {
     let email = data[a].email.toLowerCase();
-    arrEmail.push(email);
+    let nama = data[a].name.toLowerCase();
+    let city = data[a].address["city"];
+
+    const obj = {email:email,
+      name:nama,
+      city:city
+    }
+
+    arrUser.push(obj);
   }
 
-  console.log(arrEmail);
+  console.log(arrUser);
 }
 
 processUserData(await requestDataAsync());
 
-async function toLowerCase(data) {
-  let lower;
+//Check string using loop
+function checkString(data){
   let hasil = "";
+  let lower;
 
-  for (let a = 0; a < data.length; a++) {
-    let theEmail = data[a].email;
-
-    for (let b = 0; b < theEmail.length; b++) {
-      let res = theEmail[b].charCodeAt(theEmail[b]);
+  for (let b = 0; b < data.length; b++) {
+      let res = data[b].charCodeAt(data[b]);
 
       if (res >= 65 && res <= 90) {
         lower = res + 32;
@@ -56,11 +66,26 @@ async function toLowerCase(data) {
         hasil += String.fromCharCode(res);
       }
     }
-    arrEmail2.push(hasil);
-    hasil = "";
+
+    return hasil;
+}
+
+//Check lowercase without built in method
+async function toLowerCase(data) {
+  for (let a = 0; a < data.length; a++) {
+    let theEmail = data[a].email;
+    let nama = data[a].name;
+    let city = data[a].address["city"];
+
+    const user = {name:checkString(theEmail),
+      email:checkString(theEmail),
+      city:checkString(city)
+    }
+
+    arrUser2.push(user);
   }
 
-  console.log(arrEmail2);
+  console.log(arrUser2);
 }
 
 toLowerCase(await requestDataAsync());
